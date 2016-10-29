099db42e7c4f3b4e33eeac53ebbd16b063b9b151
hobbes09 <sourin.sutradhar@gmail.com>
2016-10-29 22:46:33 +0530

    Application configs

---
+++ src/main/java/com/lucille/migrations/AppConfig.java
@@ -0,0 +1,16 @@
+package com.lucille.migrations;
+
+
+public class AppConfig{
+
+	public static final String DB_HOST = "localhost";
+
+	public static final String DB_PORT = "5432";
+
+	public static final String DB_NAME = "lucille_development";
+
+	public static final String DB_USER = "postgres";
+
+	public static final String DB_PASSWORD = null;
+
+}
