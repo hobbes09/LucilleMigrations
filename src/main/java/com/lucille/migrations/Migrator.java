f762584c22393dcd4a80a9ac11725ceb2b9ac4ed
hobbes09 <sourin.sutradhar@gmail.com>
2016-10-29 22:46:50 +0530

    Migrator class

---
+++ src/main/java/com/lucille/migrations/Migrator.java
@@ -0,0 +1,17 @@
+package com.lucille.migrations;
+
+import org.flywaydb.core.Flyway;
+
+public class Migrator {
+    public static void main(String[] args) {
+        // Create the Flyway instance
+        Flyway flyway = new Flyway();
+
+        // Point it to the database
+        flyway.setDataSource("jdbc:postgresql://" + AppConfig.DB_HOST + ":" + AppConfig.DB_PORT + "/" + AppConfig.DB_NAME, AppConfig.DB_USER, AppConfig.DB_PASSWORD);
+
+        // Start the migration
+        flyway.migrate();
+    }
+}
+
